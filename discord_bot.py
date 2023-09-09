90424df0022438c2279af0ab3d16e9d85c7e1c7e
KaiTheJu <89524717+KaiTheJu@users.noreply.github.com>
2023-09-09 14:38:28 +0200

    de flasch Ort gsi, schad back to square one

--- discord_bot.py
+++ discord_bot.py
@@ -94,7 +94,6 @@ async def assign(ctx: commands.Context, *args):
         raise Exception('Team not found')
 
     assigned_players = [player for tihm in team_list for player in tihm['players']]
-    print(players)
     for player in players:
         for pleier in player_list:
             if player == pleier: break
