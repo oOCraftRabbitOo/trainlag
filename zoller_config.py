87761b689a240b3a5aa2eef9f3af8ac52b2afa30
nelipc <nelio.zoller@gmail.com>
2024-04-17 22:01:44 +0200

    config configured

--- zoller_config.py
+++ zoller_config.py
@@ -1,15 +1,31 @@
+import datetime
+
 # ==============
 # = Main Stuff =
 # ==============
 # team compositions and channels
 TEAM_FILE = 'zoller_teams.json'
 PLAYER_FILE = 'zoller_players.json'
-GENERAL_CHANNEL = 1150146980509667478
+GENERAL_CHANNEL = 1150146980509667478 # channel for game info
 
 # point calculation
-RELATIVE_STANDARD_DEVIATION = 0.1
-POINTS_PER_KAFFNESS = 90
+RELATIVE_STANDARD_DEVIATION = 0.05
+POINTS_PER_KAFFNESS = 60
 POINTS_PER_GRADE = 20
+POINTS_PER_WALKING_MINUTE = 10
+POINTS_PER_STATIONARY_MINUTE = 10
+POINTS_PER_TRAVEL_MINUTE = 10
+START_ZONE = 121
+
+# underdog system
+UNDERDOG_MULTIPLYER_PER_1000 = 0.25
+UNDERDOG_STARTING_DIFFERENCE = 1000
+
+# zonic kaffness
+POINTS_PER_CONNECTED_ZONE_LESS_THAN_6 = 15
+POINTS_PER_BAD_CONNECTIVITY_INDEX = 25
+POINTS_FOR_NO_TRAIN = 30
+POINTS_FOR_MONGUS = 50
 
 # emoji dictionary
 EMOJI = {1: ":first_place:", 2: ":second_place:", 3: ":third_place:", 4: ":four:", 5: ":five:", 6: ":six:", 7: ":seven:", 8: ":eight:", 9: ":nine:", "last": ":poo:"}
@@ -19,25 +35,14 @@ NUM_CATCHERS = 3
 
 # bounty system
 BOUNTY_BASE_POINTS = 100
+BOUNTY_START_POINTS = 250
 BOUNTY_PERCENTAGE = 0.25
 
+# perimeter system
+PERIM_MAX_KAFF = 4
+PERIM_MAX_TRAVEL_MINUTES = 30
 
-# ==============
-# = Test Stuff =
-# ==============
-'''
-# Players
-TEAM_FILE = 'test_teams.json'
-GENERAL_CHANNEL = 1113388616845107262
-
-RELATIVE_STANDARD_DEVIATION = 0.1
-POINTS_PER_KAFFNESS = 90
-POINTS_PER_GRADE = 20
-
-# Emoji Dictionary
-EMOJI = {1: ":first_place:", 2: ":second_place:", 3: ":third_place:", 4: ":four:", 5: ":five:", 6: ":six:", 7: ":seven:", 8: ":eight:", 9: ":nine:", "last": ":poo:"}
-
-NUM_CATCHERS = 3
-BOUNTY_BASE_POINTS = 100
-BOUNTY_PERCENTAGE = 0.25
-'''
+# times
+UNSPECIFIC_TIME = datetime.time(hour=16, minute=30)
+SPECIFIC_PERIOD = datetime.timedelta(minutes=15)
+PERIMETER_TIME = datetime.time(hour=15, minute=0)
