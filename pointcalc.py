e2bb62ae19e56a5332617f33608b143a5fbcc1ea
nelipc <nelio.zoller@gmail.com>
2024-08-23 19:30:00 +0200

    moved stuff to where it should be (i'm tired)

--- pointcalc.py
+++ pointcalc.py
@@ -106,16 +106,16 @@ def pointcalc(kaffness: int,
     points += challenge_points
     points += 100 if zoneable_and_zoned else 0
     points += distance_dict[current_zone][zone] * POINTS_PER_TRAVEL_MINUTE if zone is not None else 0
-    if not fixed:
-        points += (points * (delta - UNDERDOG_STARTING_DIFFERENCE) * UNDERDOG_MULTIPLYER_PER_1000 * 0.001) if delta > UNDERDOG_STARTING_DIFFERENCE else 0
-    points *= bias
-    points = int(points)
-    points = points if fixed else randomly_adjust(points)
     points += 50 if dead_end else 0
     points += int(station_distance/20)
     points += time_to_hb * 5
     if departures is not None:
         points += int((7-departures**(1/3))*32)
+    if not fixed:
+        points += (points * (delta - UNDERDOG_STARTING_DIFFERENCE) * UNDERDOG_MULTIPLYER_PER_1000 * 0.001) if delta > UNDERDOG_STARTING_DIFFERENCE else 0
+    points *= bias
+    points = int(points)
+    points = points if fixed else randomly_adjust(points)
     return points
 
 if __name__ == "__main__":
